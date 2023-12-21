4ddec222c58f55e07e68c15e7f7cc5ea3cef68bd
Yafet <ytekleab2557@conestogac.on.ca>
2023-12-21 00:25:44 -0500

    Add venv/ to gitignore

--- app.py
+++ app.py
@@ -47,4 +47,5 @@ def save_file():
         return jsonify({'error':f'Error saving file {str(e)}'})
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
