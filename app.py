b52411b83b696286fe625266cac6486059c492a4
Yafet <ytekleab2557@conestogac.on.ca>
2023-12-21 01:12:15 -0500

    Updated run method

--- app.py
+++ app.py
@@ -46,5 +46,4 @@ def save_file():
         return jsonify({'error':f'Error saving file {str(e)}'})
     
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run()
